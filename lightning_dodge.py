4af4a36cabaafb4e5a18432b9c1c2d93fe2fe023
Zheng Zhu <zhengzhu88@gmail.com>
2016-10-18 02:13:08 -0700

    Added thresholding and some broken code to display multiple windows

--- lightning_dodge.py
+++ lightning_dodge.py
@@ -1,20 +1,45 @@
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture(0)
+def main():
+    cap = cv.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        # Our operations on the frame come here
+        # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv.imshow('frame',frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+        lower_white = np.array([255,255,255])
+        upper_white = np.array([255,255,255])
 
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+        mask = cv.inRange(frame, lower_white, upper_white)
+        if cv.countNonZero(mask) == 0:
+            continue # will put servo task here
+
+        # Display the resulting frame
+        cv.imshow('frame', frame)
+        cv.imshow('mask', mask)
+        # displayWindows([frame, mask])
+
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
+
+def displayWindows(windows):
+    n = len(windows)
+    x, y, z = windows[0].shape
+    # display = 
+    print cv.namedWindow('hi', cv.WINDOW_NORMAL)
+    for j in range(y):
+        for i in range(n*x):
+            for index in range(n):
+                display[i,j] = windows[index][i - index*x, j]
+    cv.imshow('frame', display)
+
+
+main()
